09f2c6883d598004557214aafa0055a01ba07c39
Marton <martondeveloper@gmail.com>
2018-11-21 11:46:55 +0100

    Added projects

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,4 +12,18 @@ $(window).ready(function() {
 
   }, 640);
 
+  $.get("https://api.github.com/users/MartonDev/repos", function(data) {
+
+    var projects = "";
+
+    for(i = 0; i < data.length; i++) {
+
+      projects += '<a class="project" target="_blank" href="' + data[i]["html_url"] + '">' + data[i]["name"] + '<p>' + data[i]["description"] + '<br><i class="fas fa-circle"></i> ' + data[i]["language"] + '<span class="seperator"></span><i class="fas fa-star"></i> ' + data[i]["stargazers_count"] + '</p></a>';
+
+    }
+
+    $(".projects").html(projects);
+
+  });
+
 });
